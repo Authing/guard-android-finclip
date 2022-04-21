a787868daa6a746ac6ae05e22d1b5c0b75b76bd1
zhongjiahui <zhongjiahui@authing.cn>
2022-04-21 16:17:36 +0800

    update user info page

--- app/src/main/java/cn/authing/guard/UserActivity.java
+++ app/src/main/java/cn/authing/guard/UserActivity.java
@@ -1,8 +1,11 @@
 package cn.authing.guard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import cn.authing.guard.activity.UserProfileActivity;
+import cn.authing.guard.network.AuthClient;
 
 public class UserActivity extends UserProfileActivity {
 
@@ -10,6 +13,16 @@ public class UserActivity extends UserProfileActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Button btn = findViewById(R.id.btn_logout);
+        btn.setOnClickListener(v -> logout());
+    }
+
+    private void logout() {
+        AuthClient.logout((code, message, data)->{
+            Intent intent = new Intent(this, SplashActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 
 }
